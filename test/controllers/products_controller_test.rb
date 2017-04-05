dfe2b8f57020efa8e359d3e3e056fadef122deb4
Suleman Malik <sulemanmalik@sulemans-mbp.ca.kronos.com>
2017-04-05 14:32:00 -0400

    Iteration D2-D3: Cart and add cart button

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -29,11 +29,34 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(Product.last)
   end
 
-  # ...
+  test "should show product" do
+    get product_url(@product)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_product_url(@product)
+    assert_response :success
+  end
+
   test "should update product" do
     patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
   end
 
-  # ...
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      delete product_url(@product)
+    end
+
+    assert_redirected_to products_url
+  end
 end
